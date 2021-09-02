2d0b4bf3b705be266510dac9ecafd324f2db3e78
Tran Hoang Giang <tranhoanggiang5@gmail.com>
2021-09-02 11:13:02 +0700

    Blank project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 namespace App\Http\Controllers;
 set_time_limit(-1);
 
@@ -13,28 +12,11 @@ use App\Services\GenTKB;
 
 class HomeController extends Controller
 {
-    public function generateScheduler()
-    {
-//        $teachers = Teacher::get();
-//        $labs = Lab::get();
-//        $teams = Team::get();
-//        $maps = MapTeacherSubjectTeam::with('teacher', 'subject', 'team', 'subject')->get();
-//
-//        for ($i = 0; $i < 1000; $i++) {
-//            $generator = new GenTKB($teachers, $labs, $teams, $maps);
-//            $result = $generator->generateBase();
-//            if ($result != false) {
-//                break;
-//            }
-//        }
-//
-//        file_put_contents('tkb_raw.txt', json_encode($result));
-
-        $result = json_decode(file_get_contents('../ketqua'));
-        $schedule = $result->schedule;
-        $wating = $result->waiting;
-        $teams = $result->teams;
-        $score = $result->score;
-        return view('schedule2', compact('schedule', 'teams', 'score'));
+    public function generateScheduler() {
+//        $schedule = $result->schedule;
+//        $wating = $result->waiting;
+//        $teams = $result->teams;
+//        $score = $result->score;
+//        return view('schedule2', compact('schedule', 'teams', 'score'));
     }
 }
